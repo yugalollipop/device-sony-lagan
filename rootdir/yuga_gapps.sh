92462c76eef45426ce9df5f7c820a36b7070658c
Adrian Ulrich <adrian@blinkenlights.ch>
2013-11-05 19:10:26 +0100

    fix fused location provider and switch to gapps #3

--- rootdir/yuga_gapps.sh
+++ rootdir/yuga_gapps.sh
@@ -6,8 +6,8 @@
 # IF gapps is not already installed on the device
 #
 
-MD5_GAPPS="2c8845028f1352477b6233d7ba3d1e52"
-PTH_GAPPS="/data/media/0/gapps_44_yuga_2.tgz"
+MD5_GAPPS="f0167268c47d9e22e73f4b5daf5ef205"
+PTH_GAPPS="/data/media/0/gapps_44_yuga_3.tgz"
 
 # only install gapps package if md5sum matches this: (cannot use sdcard as we are not multiuser yet)
 echo "$MD5_GAPPS  $PTH_GAPPS" > /dev/.yg_gapps_md5
