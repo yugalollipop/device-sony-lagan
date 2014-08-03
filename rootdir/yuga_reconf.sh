fe1fd3d5fffb92b312560a3e9450b07bec92022c
Adrian Ulrich <adrian@blinkenlights.ch>
2014-08-03 14:41:00 +0200

    add new ondemand gov values

--- rootdir/yuga_reconf.sh
+++ rootdir/yuga_reconf.sh
@@ -25,7 +25,9 @@ case "$cfg_gov" in
     echo 1026000 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
     echo 30000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
         ;;
-    *)
+    "unmanaged")
+        ;;
+    "ondemand_old")
     echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
     echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
     echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
@@ -36,10 +38,39 @@ case "$cfg_gov" in
     echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
     echo 10 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
     echo 70 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_multi_core
-    echo 3 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential_multi_core
     echo 918000 > /sys/devices/system/cpu/cpufreq/ondemand/optimal_freq
     echo 384000 > /sys/devices/system/cpu/cpufreq/ondemand/sync_freq
     echo 80 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
+        ;;
+    "ondemand_conservative")
+    echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+    echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+    echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+    echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+    echo 40000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+    echo 95 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
+    echo 0 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+    echo 2 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+    echo 5 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
+    echo 60 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_multi_core
+    echo 810000  > /sys/devices/system/cpu/cpufreq/ondemand/optimal_freq
+    echo 810000  > /sys/devices/system/cpu/cpufreq/ondemand/sync_freq
+    echo 70 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
+        ;;
+    *)
+    echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+    echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+    echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+    echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+    echo 40000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+    echo 95 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
+    echo 0 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+    echo 2 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+    echo 5 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
+    echo 60 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_multi_core
+    echo 1026000 > /sys/devices/system/cpu/cpufreq/ondemand/optimal_freq
+    echo 1026000 > /sys/devices/system/cpu/cpufreq/ondemand/sync_freq
+    echo 70 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
         ;;
     esac
 
