70d4bc6cfcc7e59c56ac3b419166d135a39efd9d
daeiron <thomas1991_12@hotmail.com>
2014-11-08 07:47:39 +0100

    Update kernel_headers script for kma

--- update_kernel_headers.sh
+++ update_kernel_headers.sh
@@ -3,6 +3,6 @@ set -x
 
 mkdir -p kernel-headers/linux
 
-for x in msm_mdp.h msm_ion.h msm_rotator.h ; do
-	../../../bionic/libc/kernel/tools/clean_header.py -k ../../../kernel/sony/kernel_10_4_B_0_569 ../../../kernel/sony/kernel_10_4_B_0_569/include/linux/$x > kernel-headers/linux/$x
+for x in msm_mdp.h msm_ion.h msm_rotator.h msm_charm.h ; do
+	../../../bionic/libc/kernel/tools/clean_header.py -k ../../../kernel/sony/apq8064 ../../../kernel/sony/apq8064/include/linux/$x > kernel-headers/linux/$x
 done
