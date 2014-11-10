1216273c66a681799f63b86f09bacc8fc609c7c2
Adrian Ulrich <adrian@blinkenlights.ch>
2014-11-10 18:28:14 +0100

    update yginstall script

--- rootdir/yuga_gapps.sh
+++ rootdir/yuga_gapps.sh
@@ -6,8 +6,8 @@
 # IF gapps is not already installed on the device
 #
 
-MD5_GAPPS="f0167268c47d9e22e73f4b5daf5ef205"
-PTH_GAPPS="/data/media/0/gapps_44_yuga_3.tgz"
+MD5_GAPPS="db5ec0f60ad9eaab8cacc005a29d1018"
+PTH_GAPPS="/data/media/0/gapps_50_yuga_001-db5e.tgz"
 
 # only install gapps package if md5sum matches this: (cannot use sdcard as we are not multiuser yet)
 echo "$MD5_GAPPS  $PTH_GAPPS" > /dev/.yg_gapps_md5
@@ -15,14 +15,13 @@ echo "$MD5_GAPPS  $PTH_GAPPS" > /dev/.yg_gapps_md5
 cd /
 
 if ! grep -q '^tmpfs /data' /proc/mounts ; then
-    if [ ! -f /system/priv-app/SetupWizard.apk ] ; then
+    if [ ! -f /system/priv-app/GoogleOneTimeInitializer/GoogleOneTimeInitializer.apk ] ; then
         if /system/xbin/md5sum -c /dev/.yg_gapps_md5 ; then
             # ok, md5sum of file is correct: let's install gapps
             mount -o remount,rw /system
+            echo  25 > /sys/class/leds/lm3533-green/brightness
             /system/xbin/tar -xvf $PTH_GAPPS
-            sync
-            # helps calendar provider change
-            rm /data/dalvik-cache/*
+            echo 255 > /sys/class/leds/lm3533-blue/brightness
             sync
             echo s > /proc/sysrq-trigger
             echo u > /proc/sysrq-trigger
@@ -32,7 +31,8 @@ if ! grep -q '^tmpfs /data' /proc/mounts ; then
 fi
 
 # load radio-iris during boot (can not be compiled in-kernel due to sloppy coding)
-/system/bin/insmod /system/lib/modules/radio-iris-transport.ko
+## this will probably never work in android 5.0 :-(
+#/system/bin/insmod /system/lib/modules/radio-iris-transport.ko
 
 
 # tell init to continue
