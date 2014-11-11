ffb0aee908bbff271c3579b2be33a336b7dd0935
Adrian Ulrich <adrian@blinkenlights.ch>
2014-11-11 18:13:42 +0100

    Update UAPI kernel headers after build The bionic UAPI ion header does not agree with the kernel and the userland.
    We are going to simiply overwrite it after the kernel was built.

--- update_kernel_headers.sh
+++ update_kernel_headers.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -x 
 
+CLEAN_HEADERS='../../../bionic/libc/kernel/tools/clean_header.py -k ../../../kernel/sony/kernel_10_4_B_0_569 ../../../kernel/sony/kernel_10_4_B_0_569/include/linux/'
+
 mkdir -p kernel-headers/linux
 
 for x in msm_mdp.h msm_ion.h msm_rotator.h ; do
-	../../../bionic/libc/kernel/tools/clean_header.py -k ../../../kernel/sony/kernel_10_4_B_0_569 ../../../kernel/sony/kernel_10_4_B_0_569/include/linux/$x > kernel-headers/linux/$x
+	$CLEAN_HEADERS/$x > kernel-headers/linux/$x
 done
+
+# the UAPI of 5.0 expects a 3.14 (?) kernel - and does not match our definition of ion.h: fix it forcefully
+$CLEAN_HEADERS/ion.h  | sed 's/_LINUX_ION_H/_UAPI_LINUX_ION_H/' > ../../../bionic/libc/kernel/uapi/linux/ion.h
+
